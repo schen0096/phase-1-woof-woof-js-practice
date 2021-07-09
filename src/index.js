8593f73bc5a903fca72ea4286036d0449b621498
steven chen <schen0096@gmail.com>
2021-07-09 08:54:45 -0400

    Code for bonus: needs tweaking. Currently can filter on/off + get the right good dogs, but won't display properly

--- src/index.js
+++ src/index.js
@@ -41,6 +41,50 @@ function renderPup(pupData){
 
 }
 
+function filterPup(){
+    const dogFilter = document.querySelector('#good-dog-filter')
+    dogFilter.addEventListener('click', () => {
+        if (dogFilter.textContent === 'Filter good dogs: OFF'){
+            dogFilter.textContent = 'Filter good dogs: ON'
+            
+        }
+        else if (dogFilter.textContent === 'Filter good dogs: ON'){
+            dogFilter.textContent = 'Filter good dogs: OFF'
+            
+        }
+    })
+}
 
+function fetchFilterGoodPup(){
+    fetch('http://localhost:3000/pups')
+    .then(resp => resp.json())
+    .then(data => fetchFilterGoodPups(data))
+}
+
+function fetchFilterGoodPups(data){
+    data.forEach(filterGoodPup)
+}
+
+function filterGoodPup(data){
+    const dogFilter = document.querySelector('#good-dog-filter')
+    if (dogFilter.textContent = 'Filter good dogs: ON'){
+        if (data.isGoodDog === true){
+            let dogContainer = document.querySelector('#dog-bar')
+            let span = document.createElement('span')
+            span.textContent = data.name
+            dogContainer.innerHTML = ''
+            console.log(data.name)
+            dogContainer.append(span) 
+        }
+    }
+    else if (dogFilter.textContent = 'Filter good dogs: ON'){
+        let container = document.querySelector('#dog-bar')
+        let span1 = document.createElement('span')
+        span1.textContent = data.name
+        container.innerHTML = ''
+        console.log(data.name)
+        container.append(span1)
+    }
+}
 
         
